995c0d566c58d104e27966bab87c49e259ad1967
David Sargeant <dsargeant@roomkey.com>
2015-05-09 10:03:58 -0400

    Added codox to project.

--- project.clj
+++ project.clj
@@ -5,4 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
-                   :source-paths ["dev"]}})
+                   :source-paths ["dev"]
+                   :plugins [[codox "0.8.11"]]
+                   :codox {:src-dir-uri "https://github.com/dubiousdavid/system/blob/master/"
+                           :src-linenum-anchor-prefix "L"
+                           :output-dir "."
+                           :exclude [system.examples]}}})
